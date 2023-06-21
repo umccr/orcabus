460ca74da359dedf168f1787feaa260f40e8227e
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2023-06-21 13:59:22 +1000

    Add first barebones and sans-fixtures S3 file model with example query

--- skel/rust-api/src/db.rs
+++ skel/rust-api/src/db.rs
@@ -1,10 +1,9 @@
 use std::env;
-use sqlx::mysql::MySqlPool;
 
-// TODO: To be used by sqlx::query_as(), leveraging Rust's
-// type system.
-//
-// struct GDSFile {
+use sqlx::{mysql::MySqlPool, FromRow};
+
+// #[derive(Debug, FromRow)]
+// struct GDS {
 //     id: u64,
 //     path: String,
 //     // name: String,
@@ -15,14 +14,41 @@ use sqlx::mysql::MySqlPool;
 //     // time_archived: chrono::DateTime<chrono::Utc>,
 // }
 
-pub async fn query() -> Result<(), sqlx::Error> {
+#[derive(Debug, FromRow)]
+pub struct S3 {
+    pub id: i64,
+    pub bucket: String,
+    pub key: String,
+}
+
+pub async fn s3_query_something() -> Result<Vec<S3>, sqlx::Error> {
+    // TODO: Move this at "class" level instead of method level
+    let db_url = env::var("DATABASE_URL");
+    let pool = MySqlPool::connect(db_url.unwrap_or_default().as_str()).await?;
+
+    let key = "foo";
+    let objects = sqlx::query_as!(S3,
+        "
+        SELECT data_portal_s3object.id, data_portal_s3object.bucket, data_portal_s3object.key
+        FROM data_portal_s3object
+        WHERE data_portal_s3object.key = ?
+        LIMIT 10;
+        ",
+        key
+    )
+    .fetch_all(&pool).await?;
+
+    Ok(objects)
+}
+
+pub async fn plain_query() -> Result<(), sqlx::Error> {
     // TODO: Move this at "class" level instead of method level
     let db_url = env::var("DATABASE_URL");
     let pool = MySqlPool::connect(db_url.unwrap_or_default().as_str()).await?;
     
     sqlx::migrate!("./migrations").run(&pool).await?;
     
-    let res = sqlx::query("SELECT path FROM orcabus.data_portal_gdsfile")
+    let res = sqlx::query("SELECT path FROM orcabus.data_portal_gdsfile LIMIT 10")
         .fetch_one(&pool)
         .await?;
 
