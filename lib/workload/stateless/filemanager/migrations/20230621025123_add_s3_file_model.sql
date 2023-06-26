346b441878fcc7b0c7954a17698746561748c482
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2023-06-26 16:36:38 +1000

    Correct migration syntax

--- lib/workload/stateless/filemanager/migrations/20230621025123_add_s3_file_model.sql
+++ lib/workload/stateless/filemanager/migrations/20230621025123_add_s3_file_model.sql
@@ -1,10 +1,9 @@
 -- As seen in data_portal
 create table s3 (
-    id bigint not null auto_increment,
-    bucket varchar not null,
-    key longtext not null,
-    size bigint not null,
-    last_modified_date datetime not null,
-    e_tag varchar not null,
-    unique_hash varchar not null,
+    id bigint not null auto_increment primary key,
+    bucket varchar(255) not null,
+    object_key varchar(1024) not null,
+    size int not null,
+    -- last_modified_date datetime not null,
+    e_tag varchar(255) not null
 );
