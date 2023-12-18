c235c905a1dfee3d0b9d5d43e4f1eeb3566f1060
Marko Malenic <mmalenic1@gmail.com>
2023-12-18 17:42:31 +1100

    refactor(filemanager): split lambda function code

---
+++ lib/workload/stateful/filemanager/deploy/constructs/functions/function.ts
@@ -0,0 +1,130 @@
+import { IVpc, SecurityGroup, SubnetType } from 'aws-cdk-lib/aws-ec2';
+import { Database } from '../database';
+import { IQueue } from 'aws-cdk-lib/aws-sqs';
+import { Architecture, IDestination } from 'aws-cdk-lib/aws-lambda';
+import { ManagedPolicy, PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import { Construct } from 'constructs';
+import { RustFunction } from 'rust.aws-cdk-lambda';
+import { Duration } from 'aws-cdk-lib';
+import { Settings as CargoSettings } from 'rust.aws-cdk-lambda/dist/settings';
+
+/**
+ * Settable values for a Rust function.
+ */
+export type FunctionSettings = {
+  /**
+   * Additional build environment variables when building the Lambda function.
+   */
+  readonly buildEnvironment?: { [key: string]: string | undefined };
+  /**
+   * RUST_LOG string, defaults to trace on local crates and info everywhere else.
+   */
+  readonly rustLog?: string;
+};
+
+/**
+ * Props for the Rust function.
+ */
+export type FunctionProps = FunctionSettings & {
+  /**
+   * The package to build for this function.
+   */
+  readonly package: string;
+  /**
+   * Vpc for the function.
+   */
+  readonly vpc: IVpc;
+  /**
+   * Database that the function uses.
+   */
+  readonly database: Database;
+  /**
+   * The destination to post failed invocations to.
+   */
+  readonly onFailure?: IDestination;
+  /**
+   * Additional policies to add to the Lambda role.
+   */
+  readonly policies?: PolicyStatement[];
+};
+
+/**
+ * A construct for a Rust Lambda function.
+ */
+export class Function extends Construct {
+  private readonly _function: RustFunction;
+  private readonly _role: Role;
+
+  constructor(scope: Construct, id: string, props: FunctionProps) {
+    super(scope, id);
+
+    // Lambda role needs SQS execution role.
+    this._role = new Role(this, 'Role', {
+      assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+      description: 'Lambda execution role for ' + id,
+    });
+    // Lambda needs VPC access if it is created in a VPC.
+    this.addManagedPolicy('service-role/AWSLambdaVPCAccessExecutionRole');
+    props.policies?.forEach((policy) => {
+      this._role.addToPolicy(policy);
+    });
+
+    // Lambda needs to be able to reach out to access S3, security manager (eventually), etc.
+    // Could this use an endpoint instead?
+    const securityGroup = new SecurityGroup(this, 'SecurityGroup', {
+      vpc: props.vpc,
+      allowAllOutbound: true,
+      description: 'Security group that allows the ingest Lambda function to egress out.',
+    });
+
+    CargoSettings.WORKSPACE_DIR = '../';
+    CargoSettings.BUILD_INDIVIDUALLY = true;
+
+    this._function = new RustFunction(this, id, {
+      package: props.package,
+      target: 'aarch64-unknown-linux-gnu',
+      memorySize: 128,
+      timeout: Duration.seconds(28),
+      environment: {
+        // Todo use security manager to get connection string rather than passing it in as an environment variable
+        DATABASE_URL: props.database.unsafeConnection,
+        RUST_LOG:
+          props.rustLog ?? `info,${props.package.replace('-', '_')}=trace,filemanager=trace`,
+      },
+      buildEnvironment: props.buildEnvironment,
+      architecture: Architecture.ARM_64,
+      role: this._role,
+      onFailure: props.onFailure,
+      vpc: props.vpc,
+      vpcSubnets: { subnetType: SubnetType.PRIVATE_WITH_EGRESS },
+      securityGroups: [
+        securityGroup,
+        // Allow access to database.
+        props.database.securityGroup,
+      ],
+    });
+
+    // Todo this should probably connect to an RDS proxy rather than directly to the database.
+  }
+
+  /**
+   * Add a managed policy to the function's role.
+   */
+  addManagedPolicy(policyName: string) {
+    this._role.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName(policyName));
+  }
+
+  /**
+   * Get the function IAM role.
+   */
+  get role(): Role {
+    return this._role;
+  }
+
+  /**
+   * Get the Lambda function.
+   */
+  get function(): RustFunction {
+    return this._function;
+  }
+}
--- lib/workload/stateful/filemanager/deploy/constructs/functions/ingest.ts
+++ lib/workload/stateful/filemanager/deploy/constructs/functions/ingest.ts
@@ -1,14 +1,8 @@
 import { Construct } from 'constructs';
-import { IVpc, SecurityGroup, SubnetType } from 'aws-cdk-lib/aws-ec2';
 import { RustFunction } from 'rust.aws-cdk-lambda';
-import { Duration } from 'aws-cdk-lib';
-import { Architecture, IDestination } from 'aws-cdk-lib/aws-lambda';
-import { Database } from '../database';
 import { IQueue } from 'aws-cdk-lib/aws-sqs';
 import * as lambdaEventSources from 'aws-cdk-lib/aws-lambda-event-sources';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import { Settings as CargoSettings } from 'rust.aws-cdk-lambda/dist/settings';
-import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import * as fn from './function';
 
 /**
  * Settable values for the ingest function.
@@ -27,99 +21,43 @@ export type IngestFunctionSettings = {
 /**
  * Props for the database
  */
-export type IngestFunctionProps = IngestFunctionSettings & {
-  /**
-   * Vpc for the function.
-   */
-  readonly vpc: IVpc;
-  /**
-   * Database that the function uses.
-   */
-  readonly database: Database;
-  /**
-   * The SQS queue URL to receive events from.
-   */
-  readonly queue: IQueue;
-  /**
-   * The destination to post failed invocations to.
-   */
-  readonly onFailure?: IDestination;
-  /**
-   * Additional policies to add to the Lambda role.
-   */
-  readonly policies?: PolicyStatement[];
-};
+export type IngestFunctionProps = fn.FunctionSettings &
+  fn.FunctionProps & {
+    /**
+     * The SQS queue URL to receive events from.
+     */
+    readonly queue: IQueue;
+  };
 
 /**
  * A construct for the Lambda ingest function.
  */
 export class IngestFunction extends Construct {
-  private readonly _function: RustFunction;
+  private readonly _function: fn.Function;
 
   constructor(scope: Construct, id: string, props: IngestFunctionProps) {
     super(scope, id);
 
-    // Lambda role needs SQS execution role.
-    const lambdaRole = new iam.Role(this, id + 'Role', {
-      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
-      description: 'Lambda execution role for ' + id,
-    });
-    // The Lambda needs SQS read to get events from SQS.
-    lambdaRole.addManagedPolicy(
-      iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaSQSQueueExecutionRole')
-    );
-    // Lambda needs VPC access if it is created in a VPC.
-    lambdaRole.addManagedPolicy(
-      iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaVPCAccessExecutionRole')
-    );
-    props.policies?.forEach((policy) => {
-      lambdaRole.addToPolicy(policy);
-    });
-
-    // Lambda needs to be able to reach out to access S3, security manager (eventually), etc.
-    // Could this use an endpoint instead?
-    const securityGroup = new SecurityGroup(this, 'SecurityGroup', {
-      vpc: props.vpc,
-      allowAllOutbound: true,
-      description: 'Security group that allows the ingest Lambda function to egress out.',
-    });
-
-    CargoSettings.WORKSPACE_DIR = '../';
-    CargoSettings.BUILD_INDIVIDUALLY = true;
-
-    const filemanagerLambda = new RustFunction(this, 'IngestLambdaFunction', {
+    this._function = new fn.Function(this, 'IngestLambdaFunction', {
       package: 'filemanager-ingest-lambda',
-      target: 'aarch64-unknown-linux-gnu',
-      memorySize: 128,
-      timeout: Duration.seconds(28),
-      environment: {
-        // Todo use security manager to get connection string rather than passing it in as an environment variable
-        DATABASE_URL: props.database.unsafeConnection,
-        RUST_LOG: props.rustLog ?? 'info,filemanager_ingest_lambda=trace,filemanager=trace',
-      },
       buildEnvironment: props.buildEnvironment,
-      architecture: Architecture.ARM_64,
-      role: lambdaRole,
-      onFailure: props.onFailure,
+      rustLog: props.rustLog,
       vpc: props.vpc,
-      vpcSubnets: { subnetType: SubnetType.PRIVATE_WITH_EGRESS },
-      securityGroups: [
-        securityGroup,
-        // Allow access to database.
-        props.database.securityGroup,
-      ],
+      database: props.database,
+      onFailure: props.onFailure,
+      policies: props.policies,
     });
+
+    this._function.addManagedPolicy('service-role/AWSLambdaSQSQueueExecutionRole');
 
     const eventSource = new lambdaEventSources.SqsEventSource(props.queue);
-    filemanagerLambda.addEventSource(eventSource);
-
-    // Todo this should probably connect to an RDS proxy rather than directly to the database.
+    this.function.addEventSource(eventSource);
   }
 
   /**
-   * Get the Lambda function.
+   * Get the underlying Lambda function.
    */
   get function(): RustFunction {
-    return this._function;
+    return this._function.function;
   }
 }
