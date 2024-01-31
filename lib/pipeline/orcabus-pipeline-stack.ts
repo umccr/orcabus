3ba852e078626c52464e651a9dafbbaeb62dc700
William Putra Intan <61998484+williamputraintan@users.noreply.github.com>
2024-01-31 13:21:30 +1100

    Update orcabus-pipeline-stack.ts

--- lib/pipeline/orcabus-pipeline-stack.ts
+++ lib/pipeline/orcabus-pipeline-stack.ts
@@ -6,7 +6,6 @@ import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 import { OrcaBusStatelessConfig, OrcaBusStatelessStack } from '../workload/orcabus-stateless-stack';
 import { OrcaBusStatefulConfig, OrcaBusStatefulStack } from '../workload/orcabus-stateful-stack';
 import { getEnvironmentConfig } from '../../config/constants';
-import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 export class PipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: cdk.StackProps) {
@@ -14,38 +13,37 @@ export class PipelineStack extends cdk.Stack {
 
     // A connection where the pipeline get its source code
     const codeStarArn = ssm.StringParameter.valueForStringParameter(this, 'codestar_github_arn');
-    const sourceFile = pipelines.CodePipelineSource.connection('umccr/orcabus', 'main', {
-      connectionArn: codeStarArn,
+    const sourceFile = pipelines.CodePipelineSource.connection(
+      'umccr/orcabus',
+      'feature/base-cdk-deployment',
+      {
+        connectionArn: codeStarArn,
+      }
+    );
+
+    const orcabusUnitTest = new pipelines.CodeBuildStep('UnitTest', {
+      commands: ['yarn install --forzen-lockfile', 'make suite'],
+      input: sourceFile,
+      primaryOutputDirectory: '.',
     });
 
     const synthAction = new pipelines.CodeBuildStep('Synth', {
-      input: sourceFile,
-      commands: ['yarn install --frozen-lockfile', 'yarn cdk synth -v'],
+      commands: ['yarn install --frozen-lockfile', 'yarn run cdk synth -v'],
+      input: orcabusUnitTest,
       primaryOutputDirectory: 'cdk.out',
-      rolePolicyStatements: [
-        new PolicyStatement({
-          actions: ['sts:AssumeRole'],
-          resources: ['*'],
-          conditions: {
-            StringEquals: {
-              'iam:ResourceTag/aws-cdk:bootstrap-role': 'lookup',
-            },
-          },
-        }),
-      ],
     });
-    synthAction.addStepDependency(new OrcaBusTestStep('OrcaBusUnitTest', { source: sourceFile }));
 
     const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
       synth: synthAction,
       selfMutation: true,
       crossAccountKeys: true,
+      dockerEnabledForSynth: true,
       codeBuildDefaults: {
         buildEnvironment: {
-          buildImage: codebuild.LinuxBuildImage.STANDARD_6_0,
+          computeType: codebuild.ComputeType.SMALL,
+          buildImage: codebuild.LinuxArmBuildImage.AMAZON_LINUX_2_STANDARD_3_0,
         },
       },
-      dockerEnabledForSelfMutation: true,
     });
 
     /**
@@ -59,17 +57,17 @@ export class PipelineStack extends cdk.Stack {
       })
     );
 
-    /**
-     * Deployment to Gamma (Staging) account
-     */
-    const gammaConfig = getEnvironmentConfig('gamma');
-    if (!gammaConfig) throw new Error(`No 'Gamma' account configuration`);
-    pipeline.addStage(
-      new OrcaBusDeploymentStage(this, 'GammaDeployment', gammaConfig.stackProps, {
-        account: gammaConfig.accountId,
-      }),
-      { pre: [new pipelines.ManualApprovalStep('PromoteToGamma')] }
-    );
+    // /**
+    //  * Deployment to Gamma (Staging) account
+    //  */
+    // const gammaConfig = getEnvironmentConfig('gamma');
+    // if (!gammaConfig) throw new Error(`No 'Gamma' account configuration`);
+    // pipeline.addStage(
+    //   new OrcaBusDeploymentStage(this, 'GammaDeployment', gammaConfig.stackProps, {
+    //     account: gammaConfig.accountId,
+    //   }),
+    //   { pre: [new pipelines.ManualApprovalStep('PromoteToGamma')] }
+    // );
 
     /**
      * Deployment to Prod account (DISABLED)
@@ -98,41 +96,6 @@ class OrcaBusDeploymentStage extends cdk.Stage {
     super(scope, environmentName, { env: { account: env?.account, region: 'ap-southeast-2' } });
 
     new OrcaBusStatefulStack(this, 'OrcaBusStatefulStack', stackProps.orcaBusStatefulConfig);
-    new OrcaBusStatelessStack(this, 'OrcaBusStatelessStack', stackProps.orcaBusStatelessConfig);
-  }
-}
-
-interface OrcaBusTestStepProps {
-  source: pipelines.CodePipelineSource;
-}
-class OrcaBusTestStep extends pipelines.CodeBuildStep {
-  constructor(id: string, props: OrcaBusTestStepProps) {
-    const stepProps: pipelines.CodeBuildStepProps = {
-      input: props.source,
-      commands: [],
-      partialBuildSpec: codebuild.BuildSpec.fromObject({
-        env: {
-          shell: 'bash',
-        },
-        phases: {
-          install: {
-            commands: [
-              'wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-py310_23.3.1-0-Linux-x86_64.sh',
-              'bash Miniconda3-py310_23.3.1-0-Linux-x86_64.sh -b',
-              'export PATH=/root/miniconda3/bin:$PATH',
-              'conda init bash',
-              'source activate',
-              'conda create -q -n orcabus python=3.10',
-              'conda run -n orcabus yarn install',
-            ],
-          },
-          build: {
-            commands: ['conda run -n orcabus make test'],
-          },
-        },
-        version: '0.2',
-      }),
-    };
-    super(id, stepProps);
+    // new OrcaBusStatelessStack(this, 'OrcaBusStatelessStack', stackProps.orcaBusStatelessConfig);
   }
 }
