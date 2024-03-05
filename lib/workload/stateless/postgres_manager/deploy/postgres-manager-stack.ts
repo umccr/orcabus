6b9575c901acfa497d4a0ac8f484195bebc7ced0
William Putra Intan <61998484+williamputraintan@users.noreply.github.com>
2024-03-05 15:02:08 +1100

    Update postgres-manager-stack.ts

--- lib/workload/stateless/postgres_manager/deploy/postgres-manager-stack.ts
+++ lib/workload/stateless/postgres_manager/deploy/postgres-manager-stack.ts
@@ -8,6 +8,7 @@ import * as iam from 'aws-cdk-lib/aws-iam';
 import * as rds from 'aws-cdk-lib/aws-rds';
 import * as ssm from 'aws-cdk-lib/aws-ssm';
 import { MicroserviceConfig, DbAuthType } from '../function/type';
+import package_json from '../package.json';
 
 export type PostgresManagerConfig = {
   masterSecretName: string;
@@ -38,8 +39,13 @@ export class PostgresManagerStack extends Stack {
       props.clusterResourceIdParameterName
     );
 
+    const runtimeDependencies = Object.keys(package_json.dependencies);
+
     const rdsLambdaProps: nodejs.NodejsFunctionProps = {
-      bundling:{tsconfig: __dirname + '/../../../tsconfig.json'},
+      bundling: {
+        nodeModules:runtimeDependencies,
+        externalModules: runtimeDependencies
+      },
       timeout: Duration.minutes(5),
       depsLockFilePath: __dirname + '/../yarn.lock',
       handler: 'handler',
