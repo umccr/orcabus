906d8663bfd010f93cfcc5bf378ff4335d620e29
alexiswl <8197659+alexiswl@users.noreply.github.com>
2024-03-20 11:22:31 +1100

    Update cttso v2 launch stack
    Set technical tags as part of step functions API

--- lib/workload/stateless/cttso_v2_pipeline_manager/deploy/bin/deploy.ts
+++ lib/workload/stateless/cttso_v2_pipeline_manager/deploy/bin/deploy.ts
@@ -3,6 +3,7 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { ctTSOV2LaunchStateMachineStack } from '../lib/stacks/cttso_v2_launch_stack';
 import {
+  CTTSO_V2_LAUNCH_STATE_MACHINE_ARN_SSM_PARAMETER_PATH,
   ICAV2_COPY_BATCH_UTILITY_STATE_MACHINE_ARN_SSM_PARAMETER_PATH,
   ICAV2_JWT_SECRET_ARN_SSM_PARAMETER_PATH,
   SSM_PARAMETER_LIST_FOR_CTTSO_LAUNCH_LAMBDAS,
@@ -13,6 +14,7 @@ new ctTSOV2LaunchStateMachineStack(app, 'ctTSOv2LaunchStatemachineStack', {
   icav2_jwt_ssm_parameter_path: ICAV2_JWT_SECRET_ARN_SSM_PARAMETER_PATH,
   ssm_parameter_list: SSM_PARAMETER_LIST_FOR_CTTSO_LAUNCH_LAMBDAS,
   icav2_copy_batch_utility_state_machine_ssm_parameter_path: ICAV2_COPY_BATCH_UTILITY_STATE_MACHINE_ARN_SSM_PARAMETER_PATH,
+  cttso_v2_launch_state_machine_ssm_parameter_path: CTTSO_V2_LAUNCH_STATE_MACHINE_ARN_SSM_PARAMETER_PATH,
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION
