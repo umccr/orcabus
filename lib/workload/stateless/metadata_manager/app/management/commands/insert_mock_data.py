b17d29f340327c716486e1dd90e606398d343f74
William Putra Intan <61998484+williamputraintan@users.noreply.github.com>
2024-03-27 23:58:13 +1100

    test update

--- lib/workload/stateless/metadata_manager/app/management/commands/insert_mock_data.py
+++ lib/workload/stateless/metadata_manager/app/management/commands/insert_mock_data.py
@@ -1,52 +1,20 @@
+import pandas as pd
 from django.core.management import BaseCommand
-from django.core import serializers
 
-from app.models import Subject, Specimen, Library
-from app.tests.utils import insert_mock_1
-from django.core.management import call_command
+from proc.service.tracking_sheet_srv import sanitize_lab_metadata_df, persist_lab_metadata
+from proc.tests.test_tracking_sheet_srv import RECORD_1, RECORD_2, RECORD_3
 
 
 class Command(BaseCommand):
     help = "Generate mock Metadata into database for local development and testing"
 
     def handle(self, *args, **options):
-        # insert_mock_1()
+        print("insert data from proc service test")
 
-        sub_o, cre = Subject.objects.get_or_create(
-            internal_id="sub01",
-            defaults={
-                "internal_id": "sub01",
-            }
-        )
+        mock_sheet_data = [RECORD_1, RECORD_2, RECORD_3]
 
-        sub_o2, cre = Subject.objects.update_or_create(
-            internal_id="sub02",
-            defaults={
-                "internal_id": "sub02",
-            }
-        )
+        metadata_pd = pd.json_normalize(mock_sheet_data)
+        metadata_pd = sanitize_lab_metadata_df(metadata_pd)
+        result = persist_lab_metadata(metadata_pd)
 
-        spec_o, cre = Specimen.objects.get_or_create(
-            internal_id="spc1",
-            defaults={
-                "internal_id": 'spc1'
-            }
-        )
-        spec_o.subjects.add(sub_o2)
-        spec_o.subjects.add(sub_o)
-
-        last_record = spec_o.history.latest()
-
-        previous_record = last_record.prev_record
-        delta = last_record.diff_against(previous_record)
-
-        for change in delta.changes:
-            print("{} changed from {} to {}".format(change.field, change.old, change.new))
-        # de = Specimen.objects.get(internal_id="abcd")
-        # de.subjects.remove(sub_o)
-        # spec_o.subjects.add(sub_o)
-
-        # print('obj', objec)
-        # print('cre', cre)
-        # print(call_command("clean_duplicate_history", "--auto"))
-        print("done")
+        print("insert mock data completed")
