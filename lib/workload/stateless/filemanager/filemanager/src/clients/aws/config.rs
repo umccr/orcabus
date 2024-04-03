90de7778eb84fb97e568b4959c01465b01ee42bd
Marko Malenic <mmalenic1@gmail.com>
2024-04-03 12:49:05 +1100

    feat(filemanager): mock additional config methods for iam token generation

--- lib/workload/stateless/filemanager/filemanager/src/clients/aws/config.rs
+++ lib/workload/stateless/filemanager/filemanager/src/clients/aws/config.rs
@@ -1,26 +1,50 @@
-use aws_config::{defaults, BehaviorVersion, ConfigLoader, SdkConfig};
+use aws_config::{defaults, BehaviorVersion, Region, SdkConfig};
+use aws_credential_types::provider::error::CredentialsError;
+use aws_credential_types::provider::ProvideCredentials;
+use aws_credential_types::Credentials;
 use mockall::automock;
 
 /// A wrapper around a config loader.
 #[derive(Debug)]
 pub struct Config {
-    inner: ConfigLoader,
+    inner: SdkConfig,
 }
 
 #[automock]
 impl Config {
     /// Create a new config.
-    pub fn new(inner: ConfigLoader) -> Self {
+    pub fn new(inner: SdkConfig) -> Self {
         Self { inner }
     }
 
     /// Load the config.
-    pub async fn load(self) -> SdkConfig {
-        self.inner.load().await
+    pub fn load(self) -> SdkConfig {
+        self.inner
+    }
+
+    /// Get the provided credentials from the config.
+    pub async fn provide_credentials(&self) -> Option<Result<Credentials, CredentialsError>> {
+        Some(
+            self.inner
+                .credentials_provider()?
+                .provide_credentials()
+                .await,
+        )
+    }
+
+    /// Get the region used by the config.
+    // This lifetime is required because of the way automock generates the mock struct.
+    #[allow(clippy::needless_lifetimes)]
+    pub fn region<'a>(&'a self) -> Option<&'a Region> {
+        self.inner().region()
+    }
+
+    pub fn inner(&self) -> &SdkConfig {
+        &self.inner
     }
 
     /// Create a new config with default behaviour.
-    pub fn with_defaults() -> Self {
-        Self::new(defaults(BehaviorVersion::latest()))
+    pub async fn with_defaults() -> Self {
+        Self::new(defaults(BehaviorVersion::latest()).load().await)
     }
 }
--- lib/workload/stateless/filemanager/filemanager/src/database/aws/credentials.rs
+++ lib/workload/stateless/filemanager/filemanager/src/database/aws/credentials.rs
@@ -7,7 +7,6 @@ use std::time::{Duration, SystemTime};
 use crate::clients::aws::config::Config;
 use crate::error::Error::CredentialGeneratorError;
 use crate::error::Result;
-use aws_credential_types::provider::ProvideCredentials;
 use aws_sigv4::http_request::SignatureLocation::QueryParams;
 use aws_sigv4::http_request::{sign, SignableBody, SignableRequest, SigningSettings};
 use aws_sigv4::sign::v4::SigningParams;
@@ -15,23 +14,19 @@ use aws_sigv4::sign::v4::SigningParams;
 #[derive(Debug)]
 pub struct IamCredentialGenerator {
     config: Config,
-    address: String,
+    host: String,
     user: String,
 }
 
 impl IamCredentialGenerator {
     /// Create a new credential generator.
-    pub fn new(config: Config, address: String, user: String) -> Self {
-        Self {
-            config,
-            address,
-            user,
-        }
+    pub fn new(config: Config, host: String, user: String) -> Self {
+        Self { config, host, user }
     }
 
     /// Create a new credential generator with default config.
-    pub fn with_defaults(address: String, user: String) -> Self {
-        Self::new(Config::with_defaults(), address, user)
+    pub async fn with_defaults(address: String, user: String) -> Self {
+        Self::new(Config::with_defaults().await, address, user)
     }
 
     /// Get the config.
@@ -39,28 +34,27 @@ impl IamCredentialGenerator {
         &self.config
     }
 
-    /// Get the address.
-    pub fn address(&self) -> &str {
-        &self.address
+    /// Get the host address.
+    pub fn host(&self) -> &str {
+        &self.host
     }
 
-    /// Get the user
+    /// Get the user.
     pub fn user(&self) -> &str {
         &self.user
     }
 
     /// Generate an RDS IAM token which can be used as the password to connect to the RDS database.
-    pub async fn generate_iam_token(self) -> Result<String> {
-        let config = self.config.load().await;
-
-        let identity = config
-            .credentials_provider()
-            .ok_or_else(|| CredentialGeneratorError("missing credentials provider".to_string()))?
+    pub async fn generate_iam_token(&self) -> Result<String> {
+        let identity = self
+            .config
             .provide_credentials()
             .await
+            .ok_or_else(|| CredentialGeneratorError("missing credentials provider".to_string()))?
             .map_err(|err| CredentialGeneratorError(err.to_string()))?
             .into();
-        let region = config
+        let region = self
+            .config
             .region()
             .ok_or_else(|| CredentialGeneratorError("missing region".to_string()))?
             .to_string();
@@ -78,10 +72,7 @@ impl IamCredentialGenerator {
             .build()
             .map_err(|err| CredentialGeneratorError(err.to_string()))?;
 
-        let url = format!(
-            "https://{}/?Action=connect&DBUser={}",
-            self.address, self.user
-        );
+        let url = format!("https://{}/?Action=connect&DBUser={}", self.host, self.user);
 
         let signable_request = SignableRequest::new("GET", &url, empty(), SignableBody::Bytes(&[]))
             .map_err(|err| CredentialGeneratorError(err.to_string()))?;
@@ -96,5 +87,21 @@ impl IamCredentialGenerator {
         }
 
         Ok(url.to_string().split_off("https://".len()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn generate_iam_token() {
+        let generator = IamCredentialGenerator::with_defaults(
+            "127.0.0.1".to_string(),
+            "filemanager".to_string(),
+        )
+        .await;
+        let creds = generator.generate_iam_token().await;
+        println!("{:#?}", creds);
     }
 }
