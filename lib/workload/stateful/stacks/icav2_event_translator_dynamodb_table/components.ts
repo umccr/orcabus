e5d08c2e575cb57ab078f857702715ce81922b1f
Ray Liu <ray.liu@umccr.org>
2024-04-23 16:20:26 +1000

    remove dupoliacte folder

--- lib/workload/stateful/stacks/icav2_event_translator_dynamodb_table/components.ts
+++ lib/workload/stateful/stacks/icav2_event_translator_dynamodb_table/components.ts
@@ -1,12 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import { TableV2, AttributeType } from 'aws-cdk-lib/aws-dynamodb';
 
 export interface ICAv2EventTranslatorDynamoDBTableConstructProps {
   icav2EventTranslatorDynamodbTableName: string;
 }
 export class ICAv2EventTranslatorDynamoDBTableConstruct extends Construct {
-  readonly icav2EventTranslatorDynamodbTable: dynamodb.Table;
+  readonly icav2EventTranslatorDynamodbTable: TableV2;
+
   constructor(
     scope: Construct,
     id: string,
@@ -15,18 +16,18 @@ export class ICAv2EventTranslatorDynamoDBTableConstruct extends Construct {
     super(scope, id);
 
     /*
-    Initialise dynamodb table, where portal_run_id is the primary sort key
+    Initialise dynamodb table, where icav2_analysis_id is the primary sort key
     */
     this.icav2EventTranslatorDynamodbTable = this.createDynamoDBTable(props);
   }
 
   private createDynamoDBTable(props: ICAv2EventTranslatorDynamoDBTableConstructProps) {
-    return new dynamodb.Table(this, 'ICAv2EventTranslatorDynamoDBTable', {
+    return new TableV2(this, 'ICAv2EventTranslatorDynamoDBTable', {
       tableName: props.icav2EventTranslatorDynamodbTableName,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
       /* Either a db_uuid or an icav2 event id or a portal run id */
-      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
-      sortKey: { name: 'id_type', type: dynamodb.AttributeType.STRING },
+      partitionKey: { name: 'id', type: AttributeType.STRING },
+      sortKey: { name: 'id_type', type: AttributeType.STRING },
     });
   }
 }
