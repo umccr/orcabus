5729d9ad26e08ba25a1d9adcfa15ecb22d4df82a
William Putra Intan <61998484+williamputraintan@users.noreply.github.com>
2024-05-06 12:12:38 +1000

    Update migrate.py

--- lib/workload/stateless/stacks/metadata-manager/handler/migrate.py
+++ lib/workload/stateless/stacks/metadata-manager/handler/migrate.py
@@ -3,17 +3,21 @@
 
 Convenience AWS lambda handler for Django database migration command hook
 """
-from typing import Dict
-
+import logging
 from django.core.management import execute_from_command_line
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 def handler(event, context) -> dict[str, str]:
+    logger.info("Processing event:", event)
+
     resp = {
         "StackId": event.get('StackId'),
         "RequestId": event.get('RequestId'),
         "LogicalResourceId": event.get('LogicalResourceId'),
-        "PhysicalResourceId": context.get('logGroupName'),
+        "PhysicalResourceId": context.log_group_name,  # https://docs.aws.amazon.com/lambda/latest/dg/python-context
     }
 
     if event.get('RequestType') == 'Delete':
