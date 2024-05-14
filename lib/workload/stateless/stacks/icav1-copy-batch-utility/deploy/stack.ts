313eaa553c46e94c952fe5d09ff88707af69f13b
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2024-05-14 11:30:50 +1000

    Add fitting role/policy for S3 Batch Ops

--- lib/workload/stateless/stacks/icav1-copy-batch-utility/deploy/stack.ts
+++ lib/workload/stateless/stacks/icav1-copy-batch-utility/deploy/stack.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import { Duration, aws_secretsmanager } from 'aws-cdk-lib';
 import { PythonFunction } from '@aws-cdk/aws-lambda-python-alpha';
 import { Architecture, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { aws_iam as iam } from 'aws-cdk-lib';
 
 export interface ICAv1CopyBatchUtilityConfig {
   AppName: string;
@@ -24,11 +25,11 @@ export class ICAv1CopyBatchUtilityStack extends cdk.Stack {
     super(scope, id, props);
 
     // Get ICAv1 Access token secret object for construct
-    const icav1_token = aws_secretsmanager.Secret.fromSecretNameV2(
-      this,
-      'Icav1Secret',
-      props.Icav1TokenSecretId
-    );
+    // const icav1_token = aws_secretsmanager.Secret.fromSecretNameV2(
+    //   this,
+    //   'Icav1Secret',
+    //   props.Icav1TokenSecretId
+    // );
 
     // S3 Batch Ops lambda
     const lambda = new PythonFunction(this, 'ICAv1 Copy Batch Utility lambda', {
@@ -48,5 +49,51 @@ export class ICAv1CopyBatchUtilityStack extends cdk.Stack {
     });
 
     // Attach S3 Batch Operations job assumed roles and policies
+    const s3BatchCopyLambdaFunctionIamRole = new iam.Role(
+      this,
+      'S3BatchCopyLambdaFunctionIamRole',
+      {
+        assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+        managedPolicies: [
+          iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'),
+        ],
+      }
+    );
+
+    s3BatchCopyLambdaFunctionIamRole.addToPolicy(
+      new iam.PolicyStatement({
+        actions: [
+          's3:GetObject',
+          's3:GetObjectAcl',
+          's3:GetObjectTagging',
+          's3:GetObjectVersion',
+          's3:GetObjectVersionAcl',
+          's3:GetObjectVersionTagging',
+          's3:ListBucket*',
+        ],
+        resources: ['*'],
+        effect: iam.Effect.ALLOW,
+      })
+    );
+
+    s3BatchCopyLambdaFunctionIamRole.addToPolicy(
+      new iam.PolicyStatement({
+        actions: [
+          's3:PutObject',
+          's3:PutObjectAcl',
+          's3:PutObjectTagging',
+          's3:PutObjectLegalHold',
+          's3:PutObjectRetention',
+          's3:GetBucketObjectLockConfiguration',
+          's3:ListBucket*',
+          's3:GetBucketLocation',
+        ],
+        resources: [
+          `arn:aws:s3:::${props.BucketForCopyDestination}`,
+          `arn:aws:s3:::${props.BucketForCopyDestination}/*`,
+        ],
+        effect: iam.Effect.ALLOW,
+      })
+    );
   }
 }
