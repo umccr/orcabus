45176f3974bc5dd73ccf53d3d6c1e21002d10fff
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2024-05-15 10:36:10 +1000

    Clean up model factories and mock data generation

--- lib/workload/stateless/stacks/workflow-manager/workflow_manager/models/payload.py
+++ lib/workload/stateless/stacks/workflow-manager/workflow_manager/models/payload.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.serializers.json import DjangoJSONEncoder
 
 from workflow_manager.models.base import OrcaBusBaseModel, OrcaBusBaseManager
 
@@ -13,7 +14,7 @@ class Payload(OrcaBusBaseModel):
     # Definition from an external source (as known to the execution engine)
     payload_type = models.CharField(max_length=255)
     payload_ref_id = models.CharField(max_length=255, unique=True)
-    data = models.JSONField()
+    data = models.JSONField(encoder=DjangoJSONEncoder)
 
     objects = PayloadManager()
 
