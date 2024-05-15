1d755cee8b734b9c319a27be4760494a8cf8ea65
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2024-05-15 11:39:24 +1000

    Update API endpoints

--- lib/workload/stateless/stacks/workflow-manager/workflow_manager/viewsets/workflow_run.py
+++ lib/workload/stateless/stacks/workflow-manager/workflow_manager/viewsets/workflow_run.py
@@ -1,16 +1,16 @@
 from rest_framework import filters
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from workflow_manager.models.workflow import Workflow
+from workflow_manager.models.workflow_run import WorkflowRun
 from workflow_manager.pagination import StandardResultsSetPagination
-from workflow_manager.serializers import WorkflowModelSerializer
+from workflow_manager.serializers import WorkflowRunModelSerializer
 
 
-class WorkflowViewSet(ReadOnlyModelViewSet):
-    serializer_class = WorkflowModelSerializer
+class WorkflowRunViewSet(ReadOnlyModelViewSet):
+    serializer_class = WorkflowRunModelSerializer
     pagination_class = StandardResultsSetPagination
     filter_backends = [filters.OrderingFilter, filters.SearchFilter]
     ordering_fields = '__all__'
     search_fields = ordering_fields
     ordering = ['-id']
-    queryset = Workflow.objects.all()
+    queryset = WorkflowRun.objects.all()
