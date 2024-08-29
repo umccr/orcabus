87a3e629d8d93ce16fdf87d7c61d48380b637db1
Marko Malenic <mmalenic1@gmail.com>
2024-08-29 15:17:56 +1000

    feat(attributes): add main handler function

---
+++ lib/workload/stateless/stacks/attribute-annotator/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/umccr/orcabus/lib/workload/stateless/stacks/attribute-linker/schema/orcabus_workflowmanager/workflowrunstatechange"
+	"io"
+	"log/slog"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+type AttributePatch struct {
+	Op    string `json:"op"`
+	Path  string `json:"path"`
+	Value string `json:"value"`
+}
+
+func handler(event workflowrunstatechange.Event) (err error) {
+	status := strings.ToUpper(event.Detail.Status)
+	if status != "SUCCEEDED" && status != "FAILED" && status != "ABORTED" {
+		return nil
+	}
+
+	patch, err := json.Marshal(AttributePatch{
+		"add",
+		"/portalRunId",
+		event.Detail.PortalRunId,
+	})
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest(
+		"PATCH",
+		"https://file.dev.umccr.org/api/v1/s3",
+		bytes.NewBuffer(patch),
+	)
+	if err != nil {
+		return err
+	}
+
+	req.URL.RawQuery = url.Values{
+		"key": {fmt.Sprintf("*%v*", event.Detail.PortalRunId)},
+	}.Encode()
+
+	slog.Debug(fmt.Sprintf("sending attributes patch %v to %v", patch, req.URL.String()))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		err = errors.Join(resp.Body.Close())
+	}()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	slog.Debug(fmt.Sprintf("received response %v with body: %v", resp.StatusCode, body))
+
+	return nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
