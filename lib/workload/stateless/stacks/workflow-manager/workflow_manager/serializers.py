4c50acff01d5ced2e41c941b5167642c82106b2b
Ray Liu <ray.liu@umccr.org>
2024-09-09 16:01:04 +1000

    revert libraries attribute back for refernece of both orcabusid and libraryid

--- lib/workload/stateless/stacks/workflow-manager/workflow_manager/serializers.py
+++ lib/workload/stateless/stacks/workflow-manager/workflow_manager/serializers.py
@@ -39,9 +39,14 @@ class WorkflowModelSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class LibraryModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Library
+        fields = '__all__'
+
 class WorkflowRunModelSerializer(serializers.ModelSerializer):
     current_state = serializers.SerializerMethodField()
-    libraries = serializers.SerializerMethodField()
+    libraries = LibraryModelSerializer(many=True, read_only=True)
     workflow = WorkflowModelSerializer(read_only=True)
     class Meta:
         model = WorkflowRun
@@ -50,21 +55,12 @@ class WorkflowRunModelSerializer(serializers.ModelSerializer):
     def get_current_state(self, obj)->dict:
         latest_state = obj.get_latest_state()
         return StateModelSerializer(latest_state).data if latest_state else None
-    
-    def get_libraries(self, obj)->list:
-        libraries = obj.get_libraries()
-        return [library.library_id for library in libraries]
 
 class PayloadModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payload
         fields = '__all__'
 
-
-class LibraryModelSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Library
-        fields = '__all__'
 
 
 class StateModelSerializer(serializers.ModelSerializer):
