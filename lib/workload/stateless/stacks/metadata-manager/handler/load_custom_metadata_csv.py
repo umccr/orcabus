88426210d6d3141e7f94a99f8a929c24e41b66cb
William Putra Intan <61998484+williamputraintan@users.noreply.github.com>
2024-09-23 14:22:56 +1000

    Feat (MM): Custom CSV Importer for MM (#566)

---
+++ lib/workload/stateless/stacks/metadata-manager/handler/load_custom_metadata_csv.py
@@ -0,0 +1,35 @@
+import django
+import os
+import logging
+
+from libumccr import libjson
+
+from proc.service.utils import sanitize_lab_metadata_df, warn_drop_duplicated_library
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings.base')
+django.setup()
+
+from proc.service.load_csv_srv import load_metadata_csv, download_csv_to_pandas
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def handler(event, _context):
+    logger.info(f'event: {libjson.dumps(event)}')
+
+    csv_url = event.get('url', None)
+    if csv_url is None:
+        raise ValueError("URL is required")
+
+    csv_df = download_csv_to_pandas(csv_url)
+    sanitize_df = sanitize_lab_metadata_df(csv_df)
+    duplicate_clean_df = warn_drop_duplicated_library(sanitize_df)
+    result = load_metadata_csv(duplicate_clean_df)
+
+    logger.info(f'persist report: {libjson.dumps(result)}')
+    return result
+
+
+if __name__ == '__main__':
+    handler({}, {})
