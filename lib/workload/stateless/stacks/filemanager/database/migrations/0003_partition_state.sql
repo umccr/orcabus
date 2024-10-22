55b2457a75d3c4b5cc56d371ca3ce0baf92380f6
Marko Malenic <mmalenic1@gmail.com>
2024-10-22 13:20:33 +1100

    feat(filemanager): add migration for is_current_state column

---
+++ lib/workload/stateless/stacks/filemanager/database/migrations/0003_partition_state.sql
@@ -0,0 +1,22 @@
+-- Creates the `is_current_state` column to partition objects by current and historical records.
+
+-- Initially, set the `is_current_state` to false to make migrating existing data easier.
+alter table s3_object add column is_current_state boolean not null default false;
+
+-- Find the current state and update existing records.
+with to_update as (
+    -- Get all records representing the current state.
+    select * from (
+        select distinct on (bucket, key, version_id) * from s3_object
+        order by bucket, key, version_id, sequencer desc
+    ) as s3_object
+    where event_type = 'Created' and is_delete_marker = false
+)
+-- Update `is_current_state` on existing records.
+update s3_object
+set is_current_state = true
+from to_update
+where s3_object.s3_object_id = to_update.s3_object_id;
+
+-- Then, set the default to true to match new logic using `is_current_state`.
+alter table s3_object alter column is_current_state set default true;
