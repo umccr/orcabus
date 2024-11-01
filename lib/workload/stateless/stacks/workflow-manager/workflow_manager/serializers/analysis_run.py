1f88e768c3e85fc3ac89a6e4f50592c509e6b65c
Ray Liu <ray.liu@umccr.org>
2024-11-01 17:45:09 +1100

    Feat/analysis run api update (#628)
    * refactor comment and payload api
    
    * modify resolved state api method
    
    * remove pagination for wfr state and comment
    
    * add OptionalFieldsMixin for list param serializer
    
    * remove empty lines
    
    * add analysis run min serializer for anr list api
    
    * add analysis run detail content
    
    * remove redundant iport
    
    * remove redunt serializer

--- lib/workload/stateless/stacks/workflow-manager/workflow_manager/serializers/analysis_run.py
+++ lib/workload/stateless/stacks/workflow-manager/workflow_manager/serializers/analysis_run.py
@@ -1,5 +1,5 @@
 from workflow_manager.serializers.base import SerializersBase, OptionalFieldsMixin
-from workflow_manager.models import AnalysisRun, Analysis, AnalysisContext
+from workflow_manager.models import AnalysisRun
 
 class AnalysisRunBaseSerializer(SerializersBase):
     prefix = AnalysisRun.orcabus_id_prefix
@@ -10,27 +10,25 @@ class AnalysisRunListParamSerializer( OptionalFieldsMixin, AnalysisRunBaseSerial
         fields = "__all__"
 
 class AnalysisRunSerializer(AnalysisRunBaseSerializer):
+    from .analysis import AnalysisMinSerializer
+    from .analysis_context import AnalysisContextMinSerializer
+    
+    analysis = AnalysisMinSerializer(read_only=True)
+    storage_context = AnalysisContextMinSerializer(read_only=True)
+    compute_context = AnalysisContextMinSerializer(read_only=True)
     class Meta:
         model = AnalysisRun
         exclude = ["libraries"]
 
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation['analysis'] = Analysis.orcabus_id_prefix + representation['analysis']
-        if representation['storage_context']:
-            representation['storage_context'] = AnalysisContext.orcabus_id_prefix + representation['storage_context']
-        if representation['compute_context']:
-            representation['compute_context'] = AnalysisContext.orcabus_id_prefix + representation['compute_context']
-        return representation
 
 
 class AnalysisRunDetailSerializer(AnalysisRunBaseSerializer):
     from .library import LibrarySerializer
-    from .analysis import AnalysisSerializer
+    from .analysis import AnalysisDetailSerializer
     from .analysis_context import AnalysisContextSerializer
 
     libraries = LibrarySerializer(many=True, read_only=True)
-    analysis = AnalysisSerializer(read_only=True)
+    analysis = AnalysisDetailSerializer(read_only=True)
     storage_context = AnalysisContextSerializer(read_only=True)
     compute_context = AnalysisContextSerializer(read_only=True)
 
