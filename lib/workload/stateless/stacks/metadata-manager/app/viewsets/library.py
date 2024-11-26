a6fbf9972562b4b5eb4e8681d4d33593332ac8f5
william <william.intan@unimelb.edu.au>
2024-11-27 10:55:52 +1100

    Update library.py

--- lib/workload/stateless/stacks/metadata-manager/app/viewsets/library.py
+++ lib/workload/stateless/stacks/metadata-manager/app/viewsets/library.py
@@ -1,4 +1,4 @@
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework.decorators import action
 
 from app.models import Library
@@ -17,21 +17,38 @@ class LibraryViewSet(BaseViewSet):
         qs = self.queryset
         query_params = self.get_query_params()
 
-        coverage__lte = query_params.get("coverage__lte", None)
+        coverage__lte = query_params.get("coverage[lte]", None)
         if coverage__lte:
-            query_params.pop("coverage__lte")
+            query_params.pop("coverage[lte]")
             qs = qs.filter(coverage__lte=coverage__lte)
 
-        coverage__gte = query_params.get("coverage__gte", None)
+        coverage__gte = query_params.get("coverage[gte]", None)
         if coverage__gte:
-            query_params.pop("coverage__gte")
+            query_params.pop("coverage[gte]")
             qs = qs.filter(coverage__gte=coverage__gte)
+
+        project_id = query_params.get("project_id", None)
+        if project_id:
+            query_params.pop("project_id")
+            qs = qs.filter(project_set__project_id=project_id)
 
         # Continue filtering by the keys inside the library model
         return Library.objects.get_by_keyword(qs, **query_params)
 
     @extend_schema(parameters=[
-        LibrarySerializer
+        LibrarySerializer,
+        OpenApiParameter(name='coverage[lte]',
+                         description="Filter based on 'coverage' that is less than or equal to the given value.",
+                         required=False,
+                         type=float),
+        OpenApiParameter(name='coverage[gte]',
+                         description="Filter based on 'coverage' that is greater than or equal to the given value.",
+                         required=False,
+                         type=float),
+        OpenApiParameter(name='project_id',
+                         description="Filter where the associated the project has the given 'project_id'.",
+                         required=False,
+                         type=float),
     ])
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
