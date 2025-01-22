690b2464087d27cf572be38beefeb2ff79f40fc0
Ray Liu <ray.liu@umccr.org>
2025-01-23 09:37:27 +1100

    refactor srm relation orcabus id (#820)

--- lib/workload/stateless/stacks/sequence-run-manager/sequence_run_manager/models/comment.py
+++ lib/workload/stateless/stacks/sequence-run-manager/sequence_run_manager/models/comment.py
@@ -15,7 +15,7 @@ class CommentManager(OrcaBusBaseManager):
 class Comment(OrcaBusBaseModel):
     orcabus_id = OrcaBusIdField(primary_key=True, prefix='cmt')
     comment = models.TextField(null=False, blank=False)
-    association_id = models.CharField(max_length=255, null=False, blank=False)  # comment association object id
+    association_id = OrcaBusIdField(prefix='')  # comment association object id
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.CharField(max_length=255, null=False, blank=False)
     updated_at = models.DateTimeField(auto_now=True)
