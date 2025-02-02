dd89378a0889030ad1253469df4f982b0d66c003
Ray Liu <ray.liu@umccr.org>
2025-02-03 10:49:32 +1100

    remove the srv comment

--- lib/workload/stateless/stacks/sequence-run-manager/sequence_run_manager_proc/services/sequence_state_srv.py
+++ lib/workload/stateless/stacks/sequence-run-manager/sequence_run_manager_proc/services/sequence_state_srv.py
@@ -32,24 +32,4 @@ def create_sequence_state_from_bssh_event(payload: dict) -> None:
     # None by default
     comment = None
     
-    # sequence status in SUCCEEDED: 
-    # if transition from STARTED to SUCCEEDED, set comment to "Sequence complete"
-    # if transition from FAILED to SUCCEEDED, set comment to "Conversion re-triggered, and sequence completed.'
-    if sequence.status == SequenceStatus.SUCCEEDED:
-        previous_state = State.objects.filter(sequence=sequence).order_by('-timestamp').first()
-        if previous_state is not None and SequenceStatus.from_seq_run_status(previous_state.status) == SequenceStatus.STARTED:
-            comment = "Sequence completed. Now in state " + status + " ."
-        if previous_state is not None and SequenceStatus.from_seq_run_status(previous_state.status) == SequenceStatus.FAILED:
-            comment = "Conversion re-triggered, and sequence completed. Now in state " + status + " ."
-
-    # sequence status in FAILED:
-    # if transition from SUCCEEDED to FAILED, set comment to "Sequence completed. But failed in post analysis process."
-    # if transition from STARTED to FAILED, set comment to "Sequence failed. Now in state " + status + " ."
-    if sequence.status == SequenceStatus.FAILED:
-        previous_state = State.objects.filter(sequence=sequence).order_by('-timestamp').first()
-        if previous_state is not None and SequenceStatus.from_seq_run_status(previous_state.status) == SequenceStatus.SUCCEEDED:
-            comment = "Sequence completed. But failed in post analysis process."
-        else:
-            comment = "Sequence failed. Now in state " + status + " ."
-    
     State.objects.create(status=status, timestamp=timestamp, sequence=sequence, comment=comment)
