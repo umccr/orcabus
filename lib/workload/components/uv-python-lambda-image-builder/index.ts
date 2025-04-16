c7f5547e9798e60acdc89aa9f4dbef2c2034e5e1
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2025-04-16 16:13:47 +1000

    Add lambda uv python with params and secrets layer

--- lib/workload/components/uv-python-lambda-image-builder/index.ts
+++ lib/workload/components/uv-python-lambda-image-builder/index.ts
@@ -3,14 +3,19 @@ import path from 'path';
 import { PythonFunction, PythonFunctionProps } from '@aws-cdk/aws-lambda-python-alpha';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
+import {
+  ParamsAndSecretsLayerVersion,
+  ParamsAndSecretsLogLevel,
+  ParamsAndSecretsVersions,
+} from 'aws-cdk-lib/aws-lambda';
 
 export function getPythonUvDockerImage(): DockerImage {
-  return DockerImage.fromBuild(path.join(__dirname));
+  return DockerImage.fromBuild(path.join(__dirname, 'build_python'));
 }
 
 /**
- * A Python Lambda function
- */
+A Python Lambda function
+**/
 export class PythonUvFunction extends PythonFunction {
   constructor(scope: Construct, id: string, props: PythonFunctionProps) {
     const uvProps = {
@@ -24,7 +29,22 @@ export class PythonUvFunction extends PythonFunction {
             props.bundling?.buildArgs?.TARGETPLATFORM ?? lambda.Architecture.ARM_64.dockerPlatform,
         },
         image: getPythonUvDockerImage(),
+        commandHooks: {
+          beforeBundling(inputDir: string, outputDir: string): string[] {
+            return [];
+          },
+          afterBundling(inputDir: string, outputDir: string): string[] {
+            return [`rm -rf ${outputDir}/pandas/tests`];
+          },
+        },
       },
+      paramsAndSecrets:
+        props.paramsAndSecrets ??
+        ParamsAndSecretsLayerVersion.fromVersion(ParamsAndSecretsVersions.V1_0_103, {
+          cacheEnabled: true,
+          cacheSize: 300,
+          logLevel: ParamsAndSecretsLogLevel.DEBUG,
+        }),
     };
     super(scope, id, uvProps);
   }
