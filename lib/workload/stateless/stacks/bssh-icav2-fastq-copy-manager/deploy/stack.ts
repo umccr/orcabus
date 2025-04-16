d120403433b5e4744cf8f2d4bb06dd6b9c21d307
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2025-04-16 17:15:21 +1000

    Updated bssh fastq copy manager
    Fix lambda payload
    
    Use camel case for lambda inputs
    
    Fix paths, inputs and event structure

--- lib/workload/stateless/stacks/bssh-icav2-fastq-copy-manager/deploy/stack.ts
+++ lib/workload/stateless/stacks/bssh-icav2-fastq-copy-manager/deploy/stack.ts
@@ -1,127 +1,182 @@
+// **CDK and Constructs Imports**
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { BsshIcav2FastqCopyStateMachineConstruct } from './constructs/bssh-icav2-fastq-copy-manager';
+
+// **AWS Services Imports**
 import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 import * as events from 'aws-cdk-lib/aws-events';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as eventsTargets from 'aws-cdk-lib/aws-events-targets';
+import * as sfn from 'aws-cdk-lib/aws-stepfunctions';
+import { Duration } from 'aws-cdk-lib';
+import { DefinitionBody } from 'aws-cdk-lib/aws-stepfunctions';
+
+// **Path and Custom Components Imports**
 import path from 'path';
 import { PythonLambdaLayerConstruct } from '../../../../components/python-lambda-layer';
+import { PythonUvFunction } from '../../../../components/uv-python-lambda-image-builder';
 import { PythonFunction } from '@aws-cdk/aws-lambda-python-alpha';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import { Duration } from 'aws-cdk-lib';
-import { ICAv2CopyBatchUtilityConstruct } from '../../../../components/icav2-copy-files-batch';
-import { DockerImageCode, DockerImageFunction } from 'aws-cdk-lib/aws-lambda';
 
-export interface BsshIcav2FastqCopyManagerConfig {
-  /* Required external properties */
-  icav2TokenSecretId: string; // "ICAv2JWTKey-umccr-prod-service-trial"
-  /* Event Bus Configurations */
-  eventBusName: string; // OrcabusMain
-  workflowName: string; // bsshFastqCopy
-  workflowVersion: string; // 1.0.0
-  serviceVersion: string; // 2024.05.15
-  triggerLaunchSource: string; // orcabus.workflowmanager
-  internalEventSource: string; // orcabus.bsshFastqCopy
-  detailType: string; // WorkflowRunStateChange
-}
+// **Interfaces and Constants Imports**
+import {
+  BsshFastqCopyEventRuleProps,
+  BsshIcav2FastqCopyManagerConfig,
+  CreateBsshFastqCopyStateMachineProps,
+  CreateManifestLambdaFunctionProps,
+} from './interfaces';
+import { icav2DataCopySyncDetailType } from '../../../../../../config/constants';
 
 export type BsshIcav2FastqCopyManagerStackProps = BsshIcav2FastqCopyManagerConfig & cdk.StackProps;
 
 export class BsshIcav2FastqCopyManagerStack extends cdk.Stack {
-  public readonly bsshIcav2Map = {
+  public readonly bsshIcav2SfnMap = {
     prefix: 'bsshFastqCopy',
+  };
+  public readonly bsshIcav2FastqCopyEventMap = {
+    triggerSource: 'orcabus.workflowmanager',
+    triggerDetailType: 'WorkflowRunStateChange',
+    triggerDetailStatus: 'READY',
   };
 
   constructor(scope: Construct, id: string, props: BsshIcav2FastqCopyManagerStackProps) {
     super(scope, id, props);
 
     // Get the icav2 jwt secret from lookup
-    const icav2_jwt_secret_obj = secretsmanager.Secret.fromSecretNameV2(
+    const icav2AccessToken = secretsmanager.Secret.fromSecretNameV2(
       this,
       'icav2_jwt_secret',
       props.icav2TokenSecretId
     );
 
-    // // Get the lambda layer object
-    // const lambda_layer_obj = new PythonLambdaLayerConstruct(this, 'icav2_fastq_copy_lambda_layer', {
-    //   layerName: 'ICAv2FastqCopyManagerLayer',
-    //   layerDescription: 'layer to enable the fastq copy manager tools layer',
-    //   layerDirectory: path.join(__dirname, '../layers'),
-    // }).lambdaLayerVersionObj;
+    // Get eventbus object
+    const eventBusObject = events.EventBus.fromEventBusName(this, 'event_bus', props.eventBusName);
 
-    // Get Fastq List Rows lambda
-    // const bclconvert_success_event_lambda = new PythonFunction(
-    //   this,
-    //   'bclconvert_success_event_lambda_python_function',
-    //   {
-    //     functionName: `${this.bsshIcav2Map.prefix}-bclconvert-success-event-handler`,
-    //     entry: path.join(__dirname, '../lambdas/query_bclconvert_outputs_handler_py'),
-    //     runtime: lambda.Runtime.PYTHON_3_12,
-    //     architecture: lambda.Architecture.ARM_64,
-    //     index: 'query_bclconvert_outputs_handler.py',
-    //     handler: 'handler',
-    //     memorySize: 1024,
-    //     layers: [lambda_layer_obj],
-    //     timeout: Duration.seconds(60),
-    //     environment: {
-    //       ICAV2_BASE_URL: 'https://ica.illumina.com/ica/rest',
-    //       ICAV2_ACCESS_TOKEN_SECRET_ID: icav2_jwt_secret_obj.secretName,
-    //     },
-    //   }
-    // );
+    const bclconvertSuccessEventLambdaFunction = this.createManifestLambdaFunction({
+      icav2AccessToken: icav2AccessToken,
+    });
 
-    const bclconvert_success_event_lambda = new DockerImageFunction(
+    const bsshCopyStateMachine = this.createBsshFastqCopyStateMachine({
+      completionEventProps: {
+        eventSource: props.internalEventSource,
+        detailType: props.detailType,
+        workflowName: props.workflowName,
+        workflowVersion: props.workflowVersion,
+        serviceVersion: props.serviceVersion,
+      },
+      eventBus: eventBusObject,
+      icav2CopyEventProps: {
+        detailType: icav2DataCopySyncDetailType,
+      },
+      manifestLambdaFunction: bclconvertSuccessEventLambdaFunction,
+      sfnPrefix: this.bsshIcav2SfnMap.prefix,
+    });
+
+    // Add event rule to trigger the state machine
+    this.addEventRuleToTriggerStateMachine({
+      eventBusObj: eventBusObject,
+      stateMachine: bsshCopyStateMachine,
+      eventBusProps: {
+        status: this.bsshIcav2FastqCopyEventMap.triggerDetailStatus,
+        workflowName: props.workflowName,
+        eventSource: this.bsshIcav2FastqCopyEventMap.triggerSource,
+        detailType: this.bsshIcav2FastqCopyEventMap.triggerDetailType,
+      },
+    });
+  }
+
+  private createManifestLambdaFunction(props: CreateManifestLambdaFunctionProps): PythonFunction {
+    // Get the lambda layer object
+    const lambdaLayerObject = new PythonLambdaLayerConstruct(
+      this,
+      'icav2_fastq_copy_lambda_layer',
+      {
+        layerName: 'ICAv2FastqCopyManagerLayer',
+        layerDescription: 'layer to enable the fastq copy manager tools layer',
+        layerDirectory: path.join(__dirname, '../layers'),
+      }
+    ).lambdaLayerVersionObj;
+
+    const bclconvertSuccessEventHandler = new PythonUvFunction(
       this,
       'bclconvert_success_event_lambda_python_function',
       {
-        functionName: `${this.bsshIcav2Map.prefix}-bclconvert-success-event-handler`,
-        code: DockerImageCode.fromImageAsset(path.join(__dirname, '../'), {
-          file: 'lambdas/query_bclconvert_outputs_handler_py/Dockerfile',
-        }),
+        entry: path.join(__dirname, '../lambdas', 'query_bclconvert_outputs_handler_py'),
+        runtime: lambda.Runtime.PYTHON_3_12,
         architecture: lambda.Architecture.ARM_64,
+        index: 'query_bclconvert_outputs_handler.py',
+        handler: 'handler',
         timeout: Duration.seconds(900),
+        memorySize: 2048,
         environment: {
           ICAV2_BASE_URL: 'https://ica.illumina.com/ica/rest',
-          ICAV2_ACCESS_TOKEN_SECRET_ID: icav2_jwt_secret_obj.secretName,
+          ICAV2_ACCESS_TOKEN_SECRET_ID: props.icav2AccessToken.secretName,
         },
+        layers: [lambdaLayerObject],
       }
     );
 
-    // Get eventbus object
-    const event_bus_obj = events.EventBus.fromEventBusName(this, 'event_bus', props.eventBusName);
+    // Add permissions to the lambda function
+    props.icav2AccessToken.grantRead(bclconvertSuccessEventHandler.currentVersion);
 
-    // Construct the icav2 copy files state machines
-    const icav2_copy_batch_state_machine_obj = new ICAv2CopyBatchUtilityConstruct(
-      this,
-      'icav2_copy_batch_state_machine',
-      {
-        icav2JwtSecretParameterObj: icav2_jwt_secret_obj,
-        stateMachineNameSingle: `${this.bsshIcav2Map.prefix}-single-icav2-files-copy`,
-        stateMachineNameBatch: `${this.bsshIcav2Map.prefix}-batch-icav2-files-copy`,
-      }
-    );
+    // Return success event handler
+    return bclconvertSuccessEventHandler;
+  }
 
-    const icav2_bclconvert_success_event_state_machine =
-      new BsshIcav2FastqCopyStateMachineConstruct(this, id, {
-        /* Metadata */
-        prefix: this.bsshIcav2Map.prefix,
-        /* Stack objects */
-        icav2CopyBatchStateMachineObj: icav2_copy_batch_state_machine_obj.icav2CopyFilesBatchSfnObj,
-        icav2JwtSsmParameterObj: icav2_jwt_secret_obj,
-        eventBusObj: event_bus_obj,
-        /* Lambda paths */
-        bclconvertSuccessEventHandlerLambdaObj: bclconvert_success_event_lambda,
-        /* State machine paths */
-        workflowDefinitionBodyPath: path.join(
+  private createBsshFastqCopyStateMachine(
+    props: CreateBsshFastqCopyStateMachineProps
+  ): sfn.IStateMachine {
+    // Specify the statemachine and replace the arn placeholders with the lambda arns defined above
+    const bsshFastqCopyStateMachine = new sfn.StateMachine(this, 'bssh_fastq_copy_state_machine', {
+      stateMachineName: `${props.sfnPrefix}-run-icav2-fastq-copy`,
+      // defintiontemplate
+      definitionBody: DefinitionBody.fromFile(
+        path.join(
           __dirname,
           '../step_functions_templates/bclconvert_success_event_state_machine.json'
-        ),
-        // Event handling //
-        detailType: props.detailType,
-        serviceVersion: props.serviceVersion,
-        triggerLaunchSource: props.triggerLaunchSource,
-        internalEventSource: props.internalEventSource,
-        workflowName: props.workflowName,
-        workflowVersion: props.workflowVersion,
-      });
+        )
+      ),
+      // definitionSubstitutions
+      definitionSubstitutions: {
+        __bclconvert_success_event_lambda_arn__:
+          props.manifestLambdaFunction.currentVersion.functionArn,
+        __eventbus_name__: props.eventBus.eventBusName,
+        __icav2_copy_detail_type__: props.icav2CopyEventProps.detailType,
+        __detail_type__: props.completionEventProps.detailType,
+        __event_source__: props.completionEventProps.eventSource,
+        __workflow_name__: props.completionEventProps.workflowName,
+        __workflow_version__: props.completionEventProps.workflowVersion,
+        __payload_version__: props.completionEventProps.serviceVersion,
+      },
+    });
+
+    // Allow the state machine to publish to the event bus
+    props.eventBus.grantPutEventsTo(bsshFastqCopyStateMachine);
+
+    // Allow the state machine to invoke the lambda function
+    props.manifestLambdaFunction.currentVersion.grantInvoke(bsshFastqCopyStateMachine);
+
+    return bsshFastqCopyStateMachine;
+  }
+
+  private addEventRuleToTriggerStateMachine(props: BsshFastqCopyEventRuleProps) {
+    // Trigger state machine on event
+    const rule = new events.Rule(this, 'bssh_fastq_copy_trigger_rule', {
+      eventBus: props.eventBusObj,
+      eventPattern: {
+        source: [props.eventBusProps.eventSource],
+        detailType: [props.eventBusProps.detailType],
+        detail: {
+          status: [{ 'equals-ignore-case': props.eventBusProps.status }],
+          workflowName: [{ 'equals-ignore-case': props.eventBusProps.workflowName }],
+        },
+      },
+    });
+
+    // Add rule
+    rule.addTarget(
+      new eventsTargets.SfnStateMachine(props.stateMachine, {
+        input: events.RuleTargetInput.fromEventPath('$.detail'),
+      })
+    );
   }
 }
