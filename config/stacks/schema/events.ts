9d605a1b440543667578096e399beb0e12f62f52
Ray Liu <ray.liu@umccr.org>
2025-11-13 16:36:57 +1100

    remove event schema

--- config/stacks/schema/events.ts
+++ config/stacks/schema/events.ts
@@ -15,33 +15,6 @@ export const getEventSchemaStackProps = (): SchemaStackProps => {
       // adjust name, description, location accordingly
       {
         ...defaultProps,
-        schemaName: 'orcabus.sequencerunmanager@SequenceRunStateChange',
-        schemaDescription: 'State change event for sequencing run by SequenceRunManager',
-        schemaLocation: path.join(
-          __dirname,
-          docBase + '/sequencerunmanager/SequenceRunStateChange.schema.json'
-        ),
-      },
-      {
-        ...defaultProps,
-        schemaName: 'orcabus.sequencerunmanager@SequenceRunSampleSheetChange',
-        schemaDescription: 'Sample sheet change event for sequencing run by SequenceRunManager',
-        schemaLocation: path.join(
-          __dirname,
-          docBase + '/sequencerunmanager/SequenceRunSampleSheetChange.schema.json'
-        ),
-      },
-      {
-        ...defaultProps,
-        schemaName: 'orcabus.sequencerunmanager@SequenceRunLibraryLinkingChange',
-        schemaDescription: 'Library linking change event for sequencing run by SequenceRunManager',
-        schemaLocation: path.join(
-          __dirname,
-          docBase + '/sequencerunmanager/SequenceRunLibraryLinkingChange.schema.json'
-        ),
-      },
-      {
-        ...defaultProps,
         schemaName: 'orcabus.workflowmanager@WorkflowRunStateChange',
         schemaDescription: 'State change event for workflow run by WorkflowManager',
         schemaLocation: path.join(
